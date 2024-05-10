d2ded372ff7080c250fd4c896981a9cd0f972a88
CarlosVasquez30 <agrochofamultiservice@MacBook-Air-de-Agrochofa.local>
2024-05-09 23:04:26 -0400

    (rel)

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
     const axios = require('axios');
     const { ProtocolParser, parseIMEI, Data, GPRS, BufferReader } = require('complete-teltonika-parser')
     const https = require("https");
+    
 
     
     // Create a Teltonika TCP server that listens on port 5500
@@ -11,10 +12,10 @@
       console.log("New Teltonika device connected");
   
       // When a new connection is established, listen for data events
-      var imei
+      var imei;
       socket.on("data", (response) => {
           const buf = Buffer.from(response);
-          console.log({response: response.toString()})
+          console.log({response: buf})
         // Extract the source and destination IP addresses from the buffer
         const srcIp = `${buf[12]}.${buf[13]}.${buf[14]}.${buf[15]}`;
         console.log("device ip:", srcIp);
@@ -34,7 +35,31 @@
           console.log(`Sent confirmation packet ${acceptData ? "01" : "00"}`);
         }
         else {
-          let parsed = new ProtocolParser(packet);
+          let parsed;
+          try {
+            parsed = new ProtocolParser(packet);
+          } catch (error) {
+            console.error(error)
+          }
+          if (!parsed) {
+            const transformed = transformPacket(packet)
+            if (transformed) {
+              if (transformed.codecID === "0c") {
+                console.log("es Codec 12")
+                console.log({transformed})
+                console.log({ packet, imei })
+                const packetString = Buffer.from(transformed.command, 'hex').toString('utf-8');
+
+                console.log({ extractDesiredPart: extractDesiredPart(packetString) })
+                
+                sendHourmeterData({
+                  imei, dataFrame: extractDesiredPart(packetString)
+                });
+              }
+            }
+            
+            return;
+          }
           const dataLength = parsed.Content.AVL_Datas.length;
           console.log("CodecType:", parsed.CodecType);
   
@@ -151,4 +176,75 @@
     server.listen(80, () => {
       console.log("Teltonika server listening on port 80");
     });
-  })()
+})()
+  
+function transformPacket(packet) {
+  // Extraer partes del paquete
+  const preamble = packet.substring(0, 8);
+  const dataSize = packet.substring(8, 16);
+  const codecID = packet.substring(16, 18);
+  const commandQuantity1 = packet.substring(18, 20); // Ignorado
+  const type = packet.substring(20, 22); // Tipo de comando
+  const commandSize = packet.substring(22, 30); // Tamaño del comando
+  const command = packet.substring(30); // Subcadena del paquete que representa el comando en HEX
+  const commandQuantity2 = packet.substring(30, 32); // Mismo valor que commandQuantity1
+  const crc16 = packet.substring(packet.length - 8); // Valor CRC-16
+
+  // Construir el paquete transformado
+  const transformedPacket = {
+    preamble, dataSize, codecID, commandQuantity1, type, commandSize, command, commandQuantity2, crc16
+  }
+
+  return transformedPacket;
+}
+
+const http = require("http");
+
+
+function sendHourmeterData(model) {
+  
+  // Datos para la petición HTTP
+  const postData = JSON.stringify(model);
+  console.log({postData})
+  const options = {
+    hostname: 'controller.agrochofa.cl',
+    path: '/api/sga/logsHorometro/crear',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+};
+
+  // Crea la petición HTTP
+  const req = http.request(options, (res) => {
+      console.log(`Estado de la petición: ${res.statusCode}`);
+
+      res.on('data', (chunk) => {
+          console.log(`Datos recibidos del otro servidor: ${chunk}`);
+      });
+  });
+
+  req.on('error', (error) => {
+      console.error('Error al enviar la petición al otro servidor:', error);
+  });
+
+  // Envía los datos al otro servidor
+  req.write(postData);
+  req.end();
+}
+
+function extractDesiredPart(packetString) {
+  // Define la expresión regular para buscar la parte entre '>' y '<'
+  const regex = />(.*?)</;
+
+  // Ejecuta la expresión regular en la cadena
+  const match = regex.exec(packetString);
+
+  // Si se encontró una coincidencia, devuelve la parte encontrada
+  if (match && match.length > 1) {
+      return match[1];
+  } else {
+      return null; // Si no se encontró ninguna coincidencia, devuelve null o maneja el caso de error de otra manera
+  }
+}
+
